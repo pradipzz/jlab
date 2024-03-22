979b73fe5cb2572064e1eab7285dcc9ddbf72a30
pradeep09123 <pradeeps09123@gmail.com>
2024-03-23 00:14:44 +0530

    first String reverse Program

--- javaprep/src/Main.java
+++ javaprep/src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    String ret=String_rev("car");
+    System.out.println(ret);
+    }
+    public static String String_rev(String S)
+    {/* pradeep*/
+        String rc="";
+        for(int i=0;i<S.length();i++)
+        {
+            rc=S.charAt(i)+rc;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+        return rc;
+
     }
 }
