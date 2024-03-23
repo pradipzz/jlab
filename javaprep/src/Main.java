31c565f1283f4b1dc4c24dbd32f3b5c87d512b31
pradeep09123 <pradeeps09123@gmail.com>
2024-03-23 22:55:17 +0530

    rev method update

--- javaprep/src/Main.java
+++ javaprep/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
     String ret=String_rev("car");
     System.out.println(ret);
+        System.out.println(num_rev(123));
     }
     public static String String_rev(String S)
     {/* pradeep*/
@@ -16,4 +17,19 @@ public class Main {
         return rc;
 
     }
+    public static int num_rev(int num)
+    {
+       int rem;
+       int rev=0;
+       while(num!=0)
+       {
+           rem=num%10;
+           num=num/10;
+           rev=(rev*10)+rem;
+       }
+    return rev;
+    }
 }
+
+--> how to call non static method
+
