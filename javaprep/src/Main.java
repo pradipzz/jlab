6fce94dd8169a6ba349b58d0443fb2f26722c7f4
pradeep09123 <pradeeps09123@gmail.com>
2024-03-25 21:15:14 +0530

    update of Swapinning of num & Factorial

--- javaprep/src/Main.java
+++ javaprep/src/Main.java
@@ -1,10 +1,17 @@
+import com.sun.source.tree.BreakTree;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
     String ret=String_rev("car");
     System.out.println(ret);
-        System.out.println(num_rev(123));
+    Main obj1=new Main();
+    obj1.num_rev(123);
+    System.out.println(swap(1,2));
+        System.out.println(factorial(4));
     }
     public static String String_rev(String S)
     {/* pradeep*/
@@ -17,7 +24,7 @@ public class Main {
         return rc;
 
     }
-    public static int num_rev(int num)
+    public void num_rev(int num)
     {
        int rem;
        int rev=0;
@@ -27,9 +34,27 @@ public class Main {
            num=num/10;
            rev=(rev*10)+rem;
        }
-    return rev;
+        System.out.println(rev);;
+    }
+//    Swapping a number
+    public static int swap(int a,int b)
+    {
+        Scanner sc=new Scanner(System.in);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+        return 0;
+    }
+    public static int factorial(int num)
+    {
+        int fac=0;
+        for(int i=1;i<num;i++)
+        {
+            fac=fac+num*i;
+        }
+        return fac;
     }
 }
-
---> how to call non static method
 
