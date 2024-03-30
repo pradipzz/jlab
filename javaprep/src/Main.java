4777ed5f76d3b921a5609e2fbf9d70054295f471
pradeep09123 <pradeeps09123@gmail.com>
2024-03-30 19:04:59 +0530

    leap year, multiplication update

--- javaprep/src/Main.java
+++ javaprep/src/Main.java
@@ -8,34 +8,45 @@ import java.math.*;
 public class Main {
     public static void main(String[] args) {
     String ret=String_rev("car");
-    System.out.println(ret);
+    System.out.println("The reverse of String is "+ret);
     Main obj1=new Main();
     obj1.num_rev(123);
     System.out.println(swap(1,2));
-        System.out.println(factorial(4));
+        System.out.println("The factorial of given number is "+factorial(4));
         System.out.println(obj1.fibonacci(11));
         if(isprime(10))
         {
-            System.out.print(" is a prime.");
+
+            System.out.print("The num is a prime.");
         }
         else{
-            System.out.println(" is not a prime.");
+            System.out.println("The num is not a prime.");
         }
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the starting prime");
-        int start =sc.nextInt();
-        System.out.println("Enter the end prime");
-        int end=sc.nextInt();
-        for (int i=start;i<=end;i++)
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the starting prime");
+//        int start =sc.nextInt();
+//        System.out.println("Enter the end prime");
+//        int end=sc.nextInt();
+//        for (int i=start;i<=end;i++)
+//        {
+//            if(isprime(i))
+//            {
+//                System.out.println(i+" is a prime.");
+//            }
+//            else{
+//                System.out.println("null");
+//            }
+//        }
+        int s=1000;
+        int e=2024;
+        for (int i=s;i<=e;i++)
         {
-            if(isprime(i))
+            if(leap_year(i))
             {
-                System.out.println(i+" is a prime.");
-            }
-            else{
-                System.out.println("null");
-            }
+                System.out.print(" "+i);
+            };
         }
+        multiplication(9);
 
     }
     public static String String_rev(String S)
@@ -51,15 +62,18 @@ public class Main {
     }
     public void num_rev(int num)
     {
-       int rem;
-       int rev=0;
+        int count=0;
+        int rem;
+        int rev=0;
        while(num!=0)
        {
+           ++count;
            rem=num%10;
            num=num/10;
            rev=(rev*10)+rem;
        }
-        System.out.println(rev);;
+        System.out.println("The reverse of num is "+rev);
+        System.out.println("the count of number is "+count);
     }
 //    Swapping a number
     public static int swap(int a,int b)
@@ -68,12 +82,14 @@ public class Main {
         a=a+b;
         b=a-b;
         a=a-b;
+        System.out.println("The Swapping of number is");
         System.out.println("a is "+a);
         System.out.println("b is "+b);
         return 0;
     }
     public static int factorial(int num)
     {
+        System.out.println("fibonacci of "+num);
         int fac=1;
         for(int i=1;i<=num;i++)
         {
@@ -82,8 +98,9 @@ public class Main {
         return fac;
     }
     //fibonacci 0 1 1 2 3 5 8 13 21 34 55
-    public int fibonacci(int count)
+    public Integer fibonacci(int count)
     {
+        System.out.println("The fibonacci of "+count);
         int num1=0,num2=1,num3=0;
         System.out.print(num1+" "+num2);
         for(int i=2;i<count;i++)
@@ -93,7 +110,7 @@ public class Main {
            num2=num3;
             System.out.print(" "+num3+" ");
         }
-        return 0;
+        return null;
     }
     //prime
     public static boolean isprime(int num)
@@ -113,5 +130,25 @@ public class Main {
         }
         return true;
     }
+    public static boolean leap_year(int year)
+    {
+        if(((year%4==0)&&(year%100!=0))||(year%400==0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void multiplication(int mtable)
+    {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(i+"*"+mtable+"="+i*mtable);
+        }
+    }
+
+
 }
 
